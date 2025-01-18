0cabb9b1549ee5b4b855f1947611b7eccff86170
Gevesson Silva <gevesson_silva@hotmail.com>
2025-01-18 12:01:02 -0300

    Calculadora

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public enum OperacaoSelecionada
+    {
+        Soma,
+        Subtracao,
+        Multiplicacao,
+        Divisao
+    }
+
+    public class Calcular
+    {
+        public static double Soma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Subtrair(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Multipicacao(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public static double Divisao(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        double numeroAnterior, resultado;
+        OperacaoSelecionada operacaoSelecionada;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            acBoton.Click += AcBoton_Click;
+            negativoBoton.Click += NegativoBoton_Click;
+            porcentagemBoton.Click += PorcentagemBoton_Click;
+            igualBoton.Click += IgualBoton_Click;
+            pontoBoton.Click += PontoBoton_Click;
+            this.TextInput += MainWindow_TextInput; ;
+        }
+
+        private void MainWindow_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            SetResultadoLabel(int.Parse(e.Text));
+            //SetResultadoLabel(int.TryParse((e.Text) out var valor));
+        }
+
+        private void PontoBoton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!resultadoLabel.ToString().Contains(","))
+            {
+                resultadoLabel.Content = $"{resultadoLabel},";
+            }
+            
+        }
+        // melhorar a a função para implementar os atalhos das teclas
+        private void IgualBoton_Click(object sender, RoutedEventArgs e)
+        {
+            double numeroNovo;
+            if (double.TryParse(resultadoLabel.Content.ToString(), out numeroNovo))
+            {
+                switch(operacaoSelecionada)
+                {
+                    case OperacaoSelecionada.Soma:
+                        resultado = Calcular.Soma(numeroAnterior, numeroNovo);
+                        break;
+                    case OperacaoSelecionada.Subtracao:
+                        resultado = Calcular.Subtrair(numeroAnterior, numeroNovo);
+                        break;
+                    case OperacaoSelecionada.Multiplicacao:
+                        resultado = Calcular.Multipicacao(numeroAnterior, numeroNovo);
+                        break;
+                    case OperacaoSelecionada.Divisao:
+                        resultado = Calcular.Divisao(numeroAnterior, numeroNovo);
+                        break;
+                }
+                resultadoLabel.Content = $"{resultado}";
+            }
+        }
+
+        private void OperacaoBoton_Click(object sender, RoutedEventArgs e)
+        {
+            //Com isso retorno o valor exibido para 0 e salvo meu valor anterior na variável numeroAnterior
+            if (double.TryParse(resultadoLabel.Content.ToString(), out numeroAnterior))
+            {
+                resultadoLabel.Content = "0";
+            }
+            //Selecionar a operação de entrada
+            operacaoSelecionada = sender == multiplicacaoBoton ? OperacaoSelecionada.Multiplicacao : operacaoSelecionada;
+            operacaoSelecionada = sender == divisionBoton ? OperacaoSelecionada.Divisao: operacaoSelecionada;
+            operacaoSelecionada = sender == menosBoton ? OperacaoSelecionada.Subtracao : operacaoSelecionada;
+            operacaoSelecionada = sender == maisBoton ? OperacaoSelecionada.Soma : operacaoSelecionada;
+        }
+
+        private void NumeroBoton_Click(object sender, RoutedEventArgs e)
+        {
+            SetResultadoLabel(int.Parse((sender as Button).Content.ToString()));
+        }
+
+        private void SetResultadoLabel(int numero)
+        {
+            resultadoLabel.Content = resultadoLabel.Content.ToString() == "0" ? $"{numero}" : $"{resultadoLabel.Content}{numero}";
+        }
+
+        private void PorcentagemBoton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultadoLabel.Content.ToString(), out numeroAnterior))
+            {
+                numeroAnterior = numeroAnterior / 100;
+                resultadoLabel.Content = $"{numeroAnterior}";
+            }
+        }
+
+        private void NegativoBoton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultadoLabel.Content.ToString(), out numeroAnterior) && numeroAnterior != 0)
+            {
+                numeroAnterior = numeroAnterior * -1;
+                resultadoLabel.Content = $"{numeroAnterior}";
+            }
+        }
+
+        private void AcBoton_Click(object sender, RoutedEventArgs e)
+        {
+            //Exclua o resultado mostrado na tela
+            resultadoLabel.Content = "0";
+        }
+    }
+}
